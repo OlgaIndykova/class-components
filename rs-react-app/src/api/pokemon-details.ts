74b6e372043b96de4192303699e058f32fb64311
OlgaIndykova <ola1234567890dd@gmail.com>
2025-07-13 19:27:28 +0300

    refactor: add a single file for getAllPokemons function

--- rs-react-app/src/api/pokemon-details.ts
+++ rs-react-app/src/api/pokemon-details.ts
@@ -1,5 +1,7 @@
-export const getPokemonDetails = async (pokemonApi: string) => {
-  const details = await fetch(pokemonApi).then(res => res.json());
+import { getAllPokemons } from "./all-pokemons";
+
+export const getPokemonDetails = async (url: string) => {
+  const details = await getAllPokemons(url);
 
   const species = await fetch(details.species.url).then(res => res.json());
   const description = species.flavor_text_entries.find(
